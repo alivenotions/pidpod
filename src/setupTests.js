8f5b2157709eb097075b0d50727fa782c2787937
gbhavalkar <gbhavalkar@gmail.com>
2018-10-10 10:27:59 +0530

    Testing dependencies integrated for jest-styled-components

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
 import Enzyme, { shallow, render, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import 'jest-styled-components'
 
 Enzyme.configure({ adapter: new Adapter() })
 
